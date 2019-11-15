d58e73c0a6e7a577c97791f4cbfa1e6e01cedd83
Crispancho93 <34691721+Crispancho93@users.noreply.github.com>
2019-11-15 17:34:19 -0500

    cambios

---
+++ Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace core_mssql.Models
+{
+    public partial class Categoria
+    {
+        public int Idcategoria { get; set; }
+        public string NombreCategoria { get; set; }
+        public string Observacion { get; set; }
+    }
+}
---
+++ Models/administrativoContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace core_mssql.Models
+{
+    public partial class administrativoContext : DbContext
+    {
+        public administrativoContext()
+        {
+        }
+
+        public administrativoContext(DbContextOptions<administrativoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Categoria> Categoria { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseSqlServer("Server=NEWSOFT02-PC\\SQLEXPRESS;Database=administrativo;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>(entity =>
+            {
+                entity.HasKey(e => e.Idcategoria)
+                    .HasName("PK__categori__140587C75EDC82ED");
+
+                entity.ToTable("categoria");
+
+                entity.Property(e => e.Idcategoria).HasColumnName("idcategoria");
+
+                entity.Property(e => e.NombreCategoria)
+                    .IsRequired()
+                    .HasColumnName("nombre_categoria")
+                    .HasMaxLength(45)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Observacion)
+                    .HasColumnName("observacion")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
